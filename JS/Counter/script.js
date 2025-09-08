512dc1613fc8f71dad502337bd3ed33b3ede7211
LA6_mac23 <la6_mac23@The-Hidden-Genius-Project-9.local>
2025-09-08 16:48:55 -0700

    first commit

---
+++ JS/Counter/script.js
@@ -0,0 +1,5 @@
+function counter() {
+// Write yor code below. Hint: you can finish it in about 3-5 lines.
+    counter_number = document.getElementById('counter-number');
+    counter_number.innerHTML = parseInt(counter_number.innerHTML) + 1;
+}
